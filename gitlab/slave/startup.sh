71eae9c1bbfde150363feaa02f8a0cb362941ad6
Anthony Ikeda <anthony.ikeda@gmail.com>
2017-05-12 16:40:11 -0700

    Still not able to get the keys right…

--- gitlab/slave/startup.sh
+++ gitlab/slave/startup.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-cat /tmp/server_rsa.pub | ssh USER@HOST "cat >> /var/jenkins_home/.ssh/authorized_keys"
+cat /tmp/server_rsa.pub >> /var/jenkins_home/.ssh/authorized_keys
 /usr/sbin/sshd -D
